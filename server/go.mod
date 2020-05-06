8e96d69b24b746ede36147db183e934c0d6c9fd6
w-sr <worldsr.up@gmail.com>
2020-05-06 11:01:26 +0300

    Server update

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module example.com/user/hello
 
 go 1.14
+
+require (
+	github.com/pusher/pusher-http-go v4.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+// Here, we import the required packages (including Pusher)
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-func main()  {
-	fmt.Println("Hello world!")
+	pusher "github.com/pusher/pusher-http-go"
+)
+
+// Here, we register the Pusher client
+var client = pusher.Client{
+	AppID: "993424",
+	Key: "1c26e82bbab3e3492ee0",
+	Secret: "b5a4e05d6cef94254d42",
+	Cluster: "mt1",
+	Secure: true,
+}
+
+// Here, we define a user as a struct
+type user struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+// -------------------------------------------------------
+// Here, we receive a new user's details in a POST request and
+// bind it to an instance of the user struct, so that we can trigger
+// the received details over to every other user in an event over the public channel.
+// -------------------------------------------------------
+func registerNewUser(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var newUser user
+	err = json.Unmarshal(body, &newUser)
+
+	if err != nil {
+		panic(err)
+	}
+
+	client.Trigger("update", "new-user", newUser)
+
+	json.NewEncoder(rw).Encode(newUser)
+}
+
+// -------------------------------------------------------
+// Here, we authorize users so that they can subscribe to private channels
+// -------------------------------------------------------
+func pusherAuth(res http.ResponseWriter, req *http.Request) {
+
+	params, _ := ioutil.ReadAll(req.Body)
+
+	response, err := client.AuthenticatePrivateChannel(params)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(res, string(response))
+}
+
+func main() {
+
+	// Serve the static files and templates from the public directory
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	// -------------------------------------------------------
+	// Listen on these routes for new user registration and user authorization,
+	// thereafter, handle each request using the matching handler function.
+	// -------------------------------------------------------
+	http.HandleFunc("/new/user", registerNewUser)
+	http.HandleFunc("/pusher/auth", pusherAuth)
+
+	// Start executing the application on port 8090
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
